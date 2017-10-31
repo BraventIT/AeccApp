46a6bf43992e752bbf78ad92c0327372ca90fdd0
ANTONIO CABRERA TORNERO <acabrera@bravent.net>
2017-10-31 11:49:29 +0100

    #46 Crear popup para cuando la localizacion este desactivada

---
+++ src/Mobile/AeccApp/AeccApp.Core/ViewModels/NoLocationProviderPopupViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AeccApp.Core.ViewModels
+{
+    public class NoLocationProviderPopupViewModel : ViewModelBase
+    { 
+        private Command _continueToSettingsCommand;
+        public ICommand ContinueToSettingsCommand
+        {
+            get
+            {
+                return _continueToSettingsCommand ??
+                    (_continueToSettingsCommand = new Command(OnContinueToSettingsCommand));
+            }
+        }
+        private async void OnContinueToSettingsCommand()
+        {
+
+        }
+
+        private Command _closePopupCommand;
+        public ICommand ClosePopupCommand
+        {
+            get
+            {
+                return _closePopupCommand ??
+                    (_closePopupCommand = new Command(OnClosePopupCommand));
+            }
+        }
+        private async void OnClosePopupCommand()
+        {
+            await NavigationService.HidePopupAsync();
+        }
+
+
+
+    }
+}
