c53bfb32c28b16d5dff7f6dbf22d3d4f301f3b7b
Alberto Fraj Souto <alberto_fraj@hotmail.com>
2017-11-10 13:17:56 +0100

    fixed identity

--- src/Mobile/AeccApp/AeccApp.Core/ViewModels/DashboardViewModel.cs
+++ src/Mobile/AeccApp/AeccApp.Core/ViewModels/DashboardViewModel.cs
@@ -69,12 +69,12 @@ namespace AeccApp.Core.ViewModels
                      await NavigationService.ShowPopupAsync(NoLocationProviderPopupVM);
                  }
 
-                 //await ServiceLocator.EmailService.SendAsync(new AeccApi.Models.EmailMessage()
-                 //{
-                 //    To = $"{GlobalSetting.Instance.User.Email};afraj@bravent.net",
-                 //    Subject = "Prueba de envío de mensajes",
-                 //    Body = $"Esto es una prueba.\r\nSe ha enviado en nombre de: {GlobalSetting.Instance.User.Name} {GlobalSetting.Instance.User.Surname}\r\n\r\nUn saludo!! ;-)"
-                 //}, token);
+                 await ServiceLocator.EmailService.SendAsync(new Aecc.Models.EmailMessage()
+                 {
+                     To = $"{GlobalSetting.Instance.User.Email};afraj@bravent.net",
+                     Subject = "Prueba de envío de mensajes",
+                     Body = $"Esto es una prueba.\r\nSe ha enviado en nombre de: {GlobalSetting.Instance.User.Name} {GlobalSetting.Instance.User.Surname}\r\n\r\nUn saludo!! ;-)"
+                 }, token);
              });
         }
 
