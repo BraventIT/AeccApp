61775060b8251a23637fbab6b76be2481bfd2ee1
Alberto Fraj Souto <alberto_fraj@hotmail.com>
2017-11-27 20:31:48 +0100

    pruebas del chat en Windows

--- src/Mobile/AeccApp/AeccApp.Core/Views/Chat/Templates/ChooseVolunteerTemplate.xaml.cs
+++ src/Mobile/AeccApp/AeccApp.Core/Views/Chat/Templates/ChooseVolunteerTemplate.xaml.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AeccApp.Core.Views.Templates
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ChooseVolunteerTemplate : ContentView
 	{
 		public ChooseVolunteerTemplate()
 		{
 			InitializeComponent ();
-		}
+
+            VolunteersListView.ItemSelected += (sender, e) => ((ListView)sender).SelectedItem = null;
+
+        }
 	}
 }
