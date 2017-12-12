96747534c50210c589b6aaa8337253622e382366
ANTONIO CABRERA TORNERO <acabrera@bravent.net>
2017-12-12 16:05:18 +0100

    #54 Editor en chat

---
+++ src/Mobile/AeccApp/AeccApp.Core/Controls/ExpandableEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AeccApp.Core.Controls
+{
+    public class ExpandableEditor : Editor
+    {
+        public ExpandableEditor()
+        {
+            TextChanged += OnTextChanged;
+        }
+
+        ~ExpandableEditor()
+        {
+            TextChanged -= OnTextChanged;
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            InvalidateMeasure();
+        }
+    }
+}
